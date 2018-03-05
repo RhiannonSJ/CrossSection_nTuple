966f941ccc78457f9e37da95cabafd9e0e2967ee
Rhiannon Jones <rjones0211@hotmail.com>
2018-03-05 14:30:01 -0600

    Updated for v067000

--- test/build.sh
+++ test/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export FHICL_DIR=/sbnd/app/users/rsjones/LArSoft_v06_69_00/LArSoft-v06_69_00/srcs/recoparameters/recoparameters
+export FHICL_DIR=/sbnd/app/users/rsjones/LArSoft_v06_69_00/LArSoft-v06_70_00/srcs/analysistree/analysistree
 cd $MRB_BUILDDIR
 mrbsetenv
 mrb i -j4
